eafeef04e99e4c9be4b79b7ec1b36324a3879272
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-14 16:06:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const string = `ID, Name, Occupation, Age \n 42, Bruce, Knight, 41 \n 57, Bob, Fry Cook, 19 \n 63, Blaine, Quiz Master, 58 \n \
+98, Bill, Doctor's Assistant, 26`;
+
+let arry = [{id: 42, name: "bruce", occupation: "knight", age: 41},
+            {id:57, name: "bob", occupation: "fry cook", age: 19},
+            {id:63, name: "blaine", occupation: "quiz master", age: 58},
+            {id: 98, name: "bill", occupation: "doctor's assistant", age: 26}];
+
+
+
+            
+
+            let arrayOne = [1, 2, 3, 7];
+            const newArray = arrayOne.map((x => x + 2));
+            console.log(arrayOne, newArray);
+
+            
